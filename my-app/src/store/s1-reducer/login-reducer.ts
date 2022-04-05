a05b5e2c2c3ec79999e26b4a4845e118981d9e09
Evgeny <jevgenijs240693@gmail.com>
2022-04-05 22:23:27 +0100

    added login thunkCreator

--- my-app/src/store/s1-reducer/login-reducer.ts
+++ my-app/src/store/s1-reducer/login-reducer.ts
@@ -1,3 +1,5 @@
+import {authAPI, LoginParamsType} from "../../API/LoginAPI/login-api";
+import {Dispatch} from "redux";
 
 type ActionsType = ReturnType<typeof setIsLoggedInAC>
 type InitialStateType = {
@@ -19,3 +21,11 @@ export const loginReducer = (state: InitialStateType = initialState, action: Act
 
 export const setIsLoggedInAC = (value: boolean) =>
     ({type: 'login/SET-IS-LOGGED-IN', value} as const)
+
+
+export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch) => {
+    authAPI.login(data)
+        .then(res => {
+            dispatch(setIsLoggedInAC(true))
+        })
+}
