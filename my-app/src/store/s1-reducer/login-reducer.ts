5e14814f243bf38b6b4e7252631e0ef4da4ce3cc
Evgeny <jevgenijs240693@gmail.com>
2022-04-05 22:18:21 +0100

    created updated loginReducer, and created loginAC

--- my-app/src/store/s1-reducer/login-reducer.ts
+++ my-app/src/store/s1-reducer/login-reducer.ts
@@ -1,14 +1,21 @@
 
-type ActionsType = any
-type InitialStateType = any
+type ActionsType = ReturnType<typeof setIsLoggedInAC>
+type InitialStateType = {
+    isLoggedIn: boolean
+}
 
-
-const initialState: InitialStateType = {}
+const initialState: InitialStateType = {
+    isLoggedIn: false
+}
 
 export const loginReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
-
+        case "login/SET-IS-LOGGED-IN":
+            return {...state, isLoggedIn: action.value}
         default:
             return state
     }
 }
+
+export const setIsLoggedInAC = (value: boolean) =>
+    ({type: 'login/SET-IS-LOGGED-IN', value} as const)
