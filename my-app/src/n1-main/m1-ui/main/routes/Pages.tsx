3f4db0164bc37db2d9d54b7ccca37c05ded961a3
Pavel <sosnovskiy.pavel@inbox.ru>
2022-04-10 19:01:36 +0300

    Update Pages.tsx

--- my-app/src/n1-main/m1-ui/main/routes/Pages.tsx
+++ my-app/src/n1-main/m1-ui/main/routes/Pages.tsx
@@ -1,10 +1,10 @@
 import React, { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
-import { Login } from "../../../../n2-features/f1-pages/Login/Login";
-import { SetPassword } from "../../../../n2-features/f1-pages/p2-password/SetPassword";
-import { Profile } from "../../../../n2-features/f1-pages/p3-profile/Profile";
-import { Registration } from "../../../../n2-features/f1-pages/p4-register/Registration";
-import { PageNotFound } from "../../../../n2-features/f1-pages/p1-404page/Error404";
+import { Login } from "../../../../n2-features/f1-auth/Login/Login";
+import { SetPassword } from "../../../../n2-features/f1-auth/a2-password/SetPassword";
+import { Profile } from "../../../../n2-features/f1-auth/a3-profile/Profile";
+import { PageNotFound } from "../../../../n2-features/f1-auth/a1-404page/Error404";
+import { RegistrationForm } from "../../../../n2-features/f1-auth/a4-register/r1-ui/RegistrationPage";
 
 export const PATH = {
     LOGIN:"/login/",
@@ -28,7 +28,7 @@ export type PagesType = {
 export const pages: PagesType[] = [
     {_id:0, title:"main", path:"/", exact: true, page: <Navigate to={PATH.LOGIN}/>},
     {_id:1, title:"login", path: PATH.LOGIN, exact: true, page: <Login/>},
-    {_id:2, title:"register", path: PATH.REGISTER, exact: true, page: <Registration/>},
+    {_id:2, title:"register", path: PATH.REGISTER, exact: true, page: <RegistrationForm/>},
     {_id:3, title:"forgot", path: PATH.FORGOT, exact: true, page: <input/>},
     {_id:4, title:"set-pass", path: PATH.SET_PASS, params:"/:token", exact: true, page: <SetPassword/>},
     {_id:5, title:"profile", path: PATH.PROFILE, params:"/:id" ,exact: true, page: <Profile/>},
