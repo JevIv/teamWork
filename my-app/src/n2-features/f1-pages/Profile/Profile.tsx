7df5427add7e01f4fa13bfea9864fe119b9a07de
Anton689 <zizugum@gmail.com>
2022-04-13 00:35:30 +0300

    объединили API и редюсер для отрисовки колод

--- my-app/src/n2-features/f1-pages/Profile/Profile.tsx
+++ my-app/src/n2-features/f1-pages/Profile/Profile.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import {AppRootStateType} from '../../../n1-main/m2-bll/store';
 import {UserType} from '../../../API/ProfileAPI/profileAPI';
-import {Navigate, useNavigate} from 'react-router-dom';
+import {Navigate, useLocation, useNavigate} from 'react-router-dom';
 import {UserInfo} from './ProfileComponents/UserInfo';
 import {MainBar} from './ProfileComponents/MainBar';
 import style from './ProfileStyles.module.css';
 import {CardsInfo} from '../../f2-cards/c1-cards/c1-ui/CardsInfo';
+import {setPacksListTC} from '../../f2-cards/c2-packs/p2-bll/packsList-reducer';
+import {CardPacksType} from '../../f2-cards/c2-packs/p3-dal/packsListAPI';
 
-// function Navigate(props: { to: string }) {
-//     return null;
-// }
 
 export const Profile = () => {
 
-    const profile = useSelector<AppRootStateType, UserType>(state=> state.profile.profileInfo as UserType)
-    const initialized = useSelector<AppRootStateType, boolean>(state => state.login.isAuth)
+    const location = useLocation()
+    console.log(location)
+    const dispatch = useDispatch()
+
+    useEffect(()=> {
+        dispatch(setPacksListTC())
+    },[])
 
     let navigate = useNavigate();
+
+    const profile = useSelector<AppRootStateType, UserType>(state=> state.profile.profileInfo as UserType)
+    const initialized = useSelector<AppRootStateType, boolean>(state => state.login.isAuth)
+    // const packs = useSelector<AppRootStateType, CardPacksType[]>(state => state.packs.packsList)
 
     if (!initialized) {
         return <Navigate to='/login'/>
@@ -39,6 +47,7 @@ export const Profile = () => {
                 </div>
             </div>
             <div className={style.packList}>
+                <button onClick={()=>{navigate('/packslist')}}>Packs list</button>
                 <h3>My Pack List</h3>
                 <div style={{margin: '0px 48px 0 48px'}}>
                     {/*Style у input здесь временный*/}
@@ -49,9 +58,7 @@ export const Profile = () => {
                     <MainBar/>
                 </div>
 
-                <CardsInfo/>
-                <CardsInfo/>
-                <CardsInfo/>
+                {/*{packs.map(p => <CardsInfo/>)}*/}
 
                 <div>
                     1 2 3 4 5
---
+++ my-app/src/n2-features/f2-cards/c2-packs/p2-bll/packsList-reducer.ts
@@ -0,0 +1,73 @@
+import {CardPacksType, packsListAPI} from '../p3-dal/packsListAPI';
+import {Dispatch} from 'redux';
+import {setIsLoading} from '../../../../n1-main/m2-bll/s1-reducer/app-reducer';
+
+// type InitialStateType = {
+//     packsList: CardPacksType[]
+// }
+//
+// const initialState: InitialStateType = {
+//     packsList: []
+// }
+
+type InitialStateType = {
+    cardPacks: CardPacksType[]
+    cardPacksTotalCount: number
+    maxCardsCount: number
+    minCardsCount: number
+    page: number
+    pageCount: number,
+    packName: string,
+    min?: number
+    max?: number
+    sortPacks?: string
+    user_id: string
+
+}
+
+const initialState: InitialStateType = {
+    cardPacks: [],
+    cardPacksTotalCount: 0,
+    maxCardsCount: 0,
+    minCardsCount: 0,
+    page: 0,
+    pageCount: 0,
+    packName: " ",
+    min: 0,
+    max: 0,
+    sortPacks: " ",
+    user_id: "",
+
+}
+
+export const packsListReducer = (state: InitialStateType = initialState, action: ActionsPacklistType): InitialStateType => {
+    switch (action.type) {
+        case 'packsList/SET_PACKLIST':
+            return {
+                ...state, cardPacks: action.packsList
+            }
+        case "SET_SEARCH":
+            return {...state, packName: action.packName}
+        default:
+            return state
+    }
+}
+
+//Actions creators
+
+type ActionsPacklistType = SetPacksListAcType | SetSearchAcType
+
+type SetPacksListAcType = ReturnType<typeof setPacksList>
+type SetSearchAcType = ReturnType<typeof setSearchAC>
+
+export const setPacksList = (packsList:CardPacksType[])=> ({type: 'packsList/SET_PACKLIST', packsList}) as const
+export const setSearchAC = (packName: string) => ({type: "SET_SEARCH", packName}) as const
+
+//Thunks
+
+export const setPacksListTC = ()=> (dispatch: Dispatch)=>{
+    packsListAPI.getAllPacks({user_id: '624b04b587b7af66e40a1112'})
+        .then(res => {
+            dispatch(setPacksList(res))
+        })
+}
